f90259498c0ad16da8734fadd04533b98de280d3
Antoni <ajanek@student.agh.edu.pl>
2025-10-06 17:55:49 +0200

    HashSetDoPoprawy

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -6,12 +6,21 @@ public class Lotto
     private int[] tablicaLiczb = new int[6];
     private int min = 1;
     private int max = 49;
+    private boolean jestDuplikat = false;
 
     public void uzupelnijTablice()
     {
         for(int i = 0 ; i < tablicaLiczb.length ; i++)
         {
-            tablicaLiczb[i] = min + (int)(Math.random() * ((max-min) + 1));
+            int num;
+
+            do
+            {
+                num = min + (int)(Math.random() * ((max-min) + 1));
+
+
+
+            }while(jestDuplikat);
             System.out.println(tablicaLiczb[i]);
         }
     }
--- Lab1/LottoHashSet.java
+++ Lab1/LottoHashSet.java
@@ -3,15 +3,16 @@ import java.util.HashSet;
 public class LottoHashSet
 {
     private HashSet<Integer> hashTab = new HashSet<>(6);
-    public void wypelnijTablice()
-    {
-        while(hashTab.size() < 6)
-        {
-            int num = 1 + (int)(Math.random() * (49));
-            hashTab.add(num);
+    public void wypelnijTablice() {
+        int num = 1 + (int) (Math.random() * (49));
+        while (hashTab.size() < 6) {
+            if (!(hashTab.contains(num)))
+            {
+                hashTab.add(num);
+            }
+
         }
         System.out.println(hashTab);
-
     }
     public static void main(String[] args)
     {
