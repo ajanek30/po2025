c485211dfe42f518370c3d6efcb20ce456dddb7b
Antoni <ajanek@student.agh.edu.pl>
2025-10-06 17:38:28 +0200

    HashSetFinished

--- Lab1/LottoHashSet.java
+++ Lab1/LottoHashSet.java
@@ -3,21 +3,19 @@ import java.util.HashSet;
 public class LottoHashSet
 {
     private HashSet<Integer> hashTab = new HashSet<>(6);
-    public HashSet<Integer> wypelnijTablice()
+    public void wypelnijTablice()
     {
         while(hashTab.size() < 6)
         {
-
             int num = 1 + (int)(Math.random() * (49));
             hashTab.add(num);
         }
-        return hashTab;
-
+        System.out.println(hashTab);
 
     }
     public static void main(String[] args)
     {
-        HashSet<Integer> tab = new HashSet<>();
+
         LottoHashSet lotto = new LottoHashSet();
         lotto.wypelnijTablice();
 
