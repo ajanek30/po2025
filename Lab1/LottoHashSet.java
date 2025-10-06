c0997ff8402b49b70aeb1e6d891bc38c01cce025
Antoni Janek PV <ajanek@student.agh.edu.pl>
2025-10-06 21:25:49 +0200

    HashSetLottoFinished

--- Lab1/LottoHashSet.java
+++ Lab1/LottoHashSet.java
@@ -3,15 +3,23 @@ import java.util.HashSet;
 public class LottoHashSet
 {
     private HashSet<Integer> hashTab = new HashSet<>(6);
-    public void wypelnijTablice() {
-        int num = 1 + (int) (Math.random() * (49));
-        while (hashTab.size() < 6) {
-            if (!(hashTab.contains(num)))
+    private int num;
+
+    public void wypelnijTablice()
+    {
+        do {
+            num = 1 + (int) (Math.random() * (49));
+            if(hashTab.contains(num))
+            {
+                continue;
+            }
+            else
             {
                 hashTab.add(num);
             }
 
-        }
+        }while(hashTab.size() < 6);
+
         System.out.println(hashTab);
     }
     public static void main(String[] args)
